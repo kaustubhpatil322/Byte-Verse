4372e71b7f135ada93a90ac36e8f07b40266857b
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-13 02:49:37 +0530

    Update vite.config.js
    Signed-off-by: Aditya Amol Patil  <157286761+Aditya948351@users.noreply.github.com>

--- MargVedha/frontend/vite.config.js
+++ MargVedha/frontend/vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/emotion-detector-ui/',  // <-- ADD THIS LINE
+  base: '/Byte-Verse/', // ✅ GitHub Pages needs this
   plugins: [react()],
   resolve: {
     alias: {
